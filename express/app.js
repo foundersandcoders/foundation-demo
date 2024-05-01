8dc0346d30eded4312786960f65530aa0cd20df1
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-05-01 14:27:51 +0100

    Express and cypress demos added

---
+++ express/app.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { books } from './books.js';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// Middleware
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// GET routes
+
+app.get('/', (req, res) => {
+  res.send('Home Route')
+})
+
+// Books route with a dynamic param -----------------------------
+
+app.get('/books/:bookId', (req, res) => {
+  // Get value from req body
+
+
+  // Manipulate value
+
+
+  // Send response
+
+  
+});
+
+// Books route with a query -----------------------------
+
+app.get('/books', (req, res) => {
+  // Get value from req body
+
+
+
+  // Use category value
+
+
+  // Send response
+
+
+});
+
+// Post route add new book
+
+app.post('/books', (req, res) => {
+  // Todo take a book from req body and add to book object
+});
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
---
+++ express/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    title: 'The Great Gatsby',
+    pages: 180,
+    author: 'F. Scott Fitzgerald',
+    category: 'fiction',
+  },
+  {
+    id: 2,
+    title: '1984',
+    pages: 328,
+    author: 'George Orwell',
+    category: 'sci-fi',
+  },
+  {
+    id: 3,
+    title: 'To Kill a Mockingbird',
+    pages: 281,
+    author: 'Harper Lee',
+    category: 'fiction',
+  },
+];
